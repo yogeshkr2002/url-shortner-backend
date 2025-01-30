43892b9d1d9898d265e3a3280996d51d14dc5f0d
yogeshkr2002 <yogeshkumarsharma689@gmail.com>
2025-01-30 18:36:51 +0530

    cors policy change

--- index.js
+++ index.js
@@ -7,12 +7,24 @@ require("dotenv").config();
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://url-shortner-bice-xi.vercel.app/",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: (origin, callback) => {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS policy: Access denied"), false);
+      }
+    },
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(limiter);
